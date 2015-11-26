6c57b4c3341fc705b7177b7eeb2826a98498aea9
欧瑞强 <ouruiqiang@ucfgroup.com>
2015-11-26 18:05:44 +0800

    修改发现模块

--- app/src/main/java/com/xianyi/fragment/FindFragment.java
+++ app/src/main/java/com/xianyi/fragment/FindFragment.java
@@ -3,10 +3,13 @@ package com.xianyi.fragment;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +22,7 @@ import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -27,6 +31,7 @@ import com.xianyi.customviews.TitleView;
 import com.xianyi.utils.LogUtil;
 import com.xianyi.R;
 
+import java.util.Objects;
 import java.util.Random;
 
 
@@ -50,6 +55,7 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
     //    private ImageView shopCart;//购物车
     private ViewGroup anim_mask_layout;//动画层
 //    private BadgeView buyNumView;//显示购买数量的控件
+
 
     int AnimationTime = 30000;//动画时间
     int AnimationTime1 = 800;//动画时间
@@ -113,10 +119,15 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
                     int result = random.nextInt(width) + 1;// 返回[0,10)集合中的整数，注意不包括10
 
                     int[] start_location = new int[2];
-                    start_location[0] = result - 80;
+                    start_location[0] = result;
+                    if (start_location[0] < 80) {
+                        start_location[0] = 80;
+                    } else if (start_location[0] > width) {
+                        start_location[0] = width - 160;
+                    }
                     start_location[1] = height - 80;
                     ImageView qiqiu = new ImageView(mContext);// buyImg是动画的图片，我的是一个小球（R.drawable.sign）
-                    qiqiu.setImageResource(R.drawable.bg_chat_dis_active);// 设置buyImg的图片
+                    qiqiu.setImageResource(R.drawable.balloon);// 设置buyImg的图片
                     setAnim(qiqiu, start_location);// 开始执行动画
                     mHandler.sendEmptyMessageDelayed(1, 10000);
                     break;
@@ -124,6 +135,10 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
                     if (animation_rootlayout != null && animation_rootlayout.getChildCount() > 0) {
                         for (int i = 0; i < animation_rootlayout.getChildCount(); i++) {
                             View view = animation_rootlayout.getChildAt(i);
+                            Object tag = view.getTag();
+                            if (tag != null && tag.equals("1")) {
+                                continue;
+                            }
                             FrameLayout.LayoutParams lp = (FrameLayout.LayoutParams) view.getLayoutParams();
                             lp.topMargin = lp.topMargin - 2;
                             view.setLayoutParams(lp);
@@ -132,7 +147,6 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
                             }
                         }
                     }
-                    LogUtil.d("11");
                     animation_rootlayout.invalidate();
                     mHandler.sendEmptyMessageDelayed(2, 50);
                     break;
@@ -164,9 +178,9 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
             if (animation_rootlayout != null) {
                 mHandler.removeMessages(1);
                 mHandler.removeMessages(2);
-                if(animation_rootlayout != null && animation_rootlayout.getChildCount()==0) {
+                if (animation_rootlayout != null && animation_rootlayout.getChildCount() == 0) {
                     mHandler.sendEmptyMessage(1);
-                }else{
+                } else {
                     mHandler.sendEmptyMessageDelayed(1, 10000);
                 }
                 mHandler.sendEmptyMessage(2);
@@ -197,25 +211,59 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
         }
     }
 
-    /**
-     * @param
-     * @return void
-     * @throws
-     * @Description: 创建动画层
-     */
-    private ViewGroup createAnimLayout() {
-        ViewGroup rootView = (ViewGroup) mContext.getWindow().getDecorView();
-        LinearLayout animLayout = new LinearLayout(mContext);
-        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.MATCH_PARENT);
-        animLayout.setLayoutParams(lp);
-        animLayout.setId(Integer.MAX_VALUE);
-        animLayout.setBackgroundResource(android.R.color.transparent);
-        rootView.addView(animLayout);
-        return animLayout;
+    private void showPopUp(final View v) {
+
+        LinearLayout relativeLayout = (LinearLayout) LayoutInflater.from(mContext).inflate(R.layout.find_pop_layout, null);
+        PopupWindow popupWindow = new PopupWindow(relativeLayout,
+                width*2/3, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        popupWindow.setFocusable(true);
+        popupWindow.setOutsideTouchable(true);
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+
+        int[] location = new int[2];
+        v.getLocationOnScreen(location);
+        int w = View.MeasureSpec.makeMeasureSpec(0,
+                View.MeasureSpec.UNSPECIFIED);
+        int h = View.MeasureSpec.makeMeasureSpec(0,
+                View.MeasureSpec.UNSPECIFIED);
+        relativeLayout.measure(w, h);
+        int height = relativeLayout.getMeasuredHeight();
+        int width = relativeLayout.getMeasuredWidth();
+        int x= location[0]+50;
+        int y=location[1] - height-50;
+        if(y<100){
+            y=100;
+        }
+        popupWindow.showAtLocation(v, Gravity.NO_GRAVITY, x, y);
+        popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                animation_rootlayout.removeView(v);
+            }
+        });
+//
     }
 
+    //    /**
+//     * @param
+//     * @return void
+//     * @throws
+//     * @Description: 创建动画层
+//     */
+//    private ViewGroup createAnimLayout() {
+//        ViewGroup rootView = (ViewGroup) mContext.getWindow().getDecorView();
+//        LinearLayout animLayout = new LinearLayout(mContext);
+//        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+//                LinearLayout.LayoutParams.MATCH_PARENT,
+//                LinearLayout.LayoutParams.MATCH_PARENT);
+//        animLayout.setLayoutParams(lp);
+//        animLayout.setId(Integer.MAX_VALUE);
+//        animLayout.setBackgroundResource(android.R.color.transparent);
+//        rootView.addView(animLayout);
+//        return animLayout;
+//    }
+//
     private void setAnim(final View mView, int[] start_location) {
 //        if (anim_mask_layout == null) {
 //        anim_mask_layout = null;
@@ -266,7 +314,9 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
             public void onClick(View view) {
                 Toast.makeText(mContext, "点击气球", Toast.LENGTH_SHORT).show();
                 LogUtil.d("111animation_rootlayout.getHeight():" + animation_rootlayout.getChildCount());
-                animation_rootlayout.removeView(mView);
+                mView.setTag("1");
+                showPopUp(view);
+//                animation_rootlayout.removeView(mView);
 //                int[] start_location = new int[2];// 一个整型数组，用来存储按钮的在屏幕的X、Y坐标
 //                mView.getLocationInWindow(start_location);// 这是获取购买按钮的在屏幕的X、Y坐标（这也是动画开始的坐标）
 //                Log.d("ouou", "start_location[0]:" + start_location[0]);
@@ -275,21 +325,24 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
 //                buyImg.setImageResource(R.drawable.sign);// 设置buyImg的图片
 //                setAnim_AddTop(buyImg, start_location);
 //                translateAnimationY.cancel();
-                mView.setVisibility(View.GONE);
-                LogUtil.d("222animation_rootlayout.getHeight():" + animation_rootlayout.getChildCount());
-                int count = animation_rootlayout.getChildCount();
-                for (int i = 0; i < count; i++) {
+//                mView.setVisibility(View.GONE);
 
-                    View qqview = animation_rootlayout.getChildAt(i);
-                    int[] location = new int[2];// 这是用来存储动画结束位置的X、Y坐标
-                    qqview.getLocationInWindow(location);// shopCart是那个购物车
-                    LogUtil.d("位置:" + location[0] + "     " + location[1]);
 
-                }
+//                LogUtil.d("222animation_rootlayout.getHeight():" + animation_rootlayout.getChildCount());
+//                int count = animation_rootlayout.getChildCount();
+//                for (int i = 0; i < count; i++) {
+//
+//                    View qqview = animation_rootlayout.getChildAt(i);
+//                    int[] location = new int[2];// 这是用来存储动画结束位置的X、Y坐标
+//                    qqview.getLocationInWindow(location);// shopCart是那个购物车
+//                    LogUtil.d("位置:" + location[0] + "     " + location[1]);
+//
+//                }
             }
         });
     }
 
+    //
     private View addViewToAnimLayout(final View view,
                                      int[] location) {
         int x = location[0];
@@ -304,63 +357,63 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
         view.setLayoutParams(lp);
         return view;
     }
-
-    private void setAnim_AddTop(final View v, int[] start_location) {
-//        if (anim_mask_layout == null) {
-        anim_mask_layout = null;
-        anim_mask_layout = createAnimLayout();
-        anim_mask_layout.addView(v);//把动画小球添加到动画层
-//        }
-        View view = addViewToAnimLayout(v,
-                start_location);
-        int[] end_location = new int[2];// 这是用来存储动画结束位置的X、Y坐标
-        titleView.getLocationInWindow(end_location);// shopCart是那个购物车
-        Log.d("ouou", "##end_location[1]:" + end_location[1]);
-        // 计算位移
-        int endX = start_location[0];// 动画位移的X坐标
-        Log.d("ouou", "##endX:" + endX);
-        int endY = end_location[1] - start_location[1];// 动画位移的y坐标
-        TranslateAnimation translateAnimationX = new TranslateAnimation(0,
-                endX, 0, 0);
-        translateAnimationX.setInterpolator(new LinearInterpolator());
-        translateAnimationX.setRepeatCount(0);// 动画重复执行的次数
-        translateAnimationX.setFillAfter(true);
-
-        TranslateAnimation translateAnimationY = new TranslateAnimation(0, 0,
-                0, endY);
-        translateAnimationY.setInterpolator(new AccelerateInterpolator());//速率
-//        translateAnimationY.setInterpolator(new LinearInterpolator());//速率
-        translateAnimationY.setRepeatCount(0);// 动画重复执行的次数
-        translateAnimationX.setFillAfter(true);
-
-        AnimationSet set = new AnimationSet(false);
-        set.setFillAfter(false);
-        set.addAnimation(translateAnimationY);
-        set.addAnimation(translateAnimationX);
-        set.setDuration(AnimationTime1);// 动画的执行时间
-        view.startAnimation(set);
-        // 动画监听事件
-        set.setAnimationListener(new Animation.AnimationListener() {
-            // 动画的开始
-            @Override
-            public void onAnimationStart(Animation animation) {
-                v.setVisibility(View.VISIBLE);
-            }
-
-            @Override
-            public void onAnimationRepeat(Animation animation) {
-                // TODO Auto-generated method stub
-            }
-
-            // 动画的结束
-            @Override
-            public void onAnimationEnd(Animation animation) {
-                v.setVisibility(View.GONE);
-//                buyNumView.setText(buyNum + "");//
-//                buyNumView.setBadgePosition(BadgeView.POSITION_TOP_RIGHT);
-//                buyNumView.show();
-            }
-        });
-    }
+//
+//    private void setAnim_AddTop(final View v, int[] start_location) {
+////        if (anim_mask_layout == null) {
+//        anim_mask_layout = null;
+//        anim_mask_layout = createAnimLayout();
+//        anim_mask_layout.addView(v);//把动画小球添加到动画层
+////        }
+//        View view = addViewToAnimLayout(v,
+//                start_location);
+//        int[] end_location = new int[2];// 这是用来存储动画结束位置的X、Y坐标
+//        titleView.getLocationInWindow(end_location);// shopCart是那个购物车
+//        Log.d("ouou", "##end_location[1]:" + end_location[1]);
+//        // 计算位移
+//        int endX = start_location[0];// 动画位移的X坐标
+//        Log.d("ouou", "##endX:" + endX);
+//        int endY = end_location[1] - start_location[1];// 动画位移的y坐标
+//        TranslateAnimation translateAnimationX = new TranslateAnimation(0,
+//                endX, 0, 0);
+//        translateAnimationX.setInterpolator(new LinearInterpolator());
+//        translateAnimationX.setRepeatCount(0);// 动画重复执行的次数
+//        translateAnimationX.setFillAfter(true);
+//
+//        TranslateAnimation translateAnimationY = new TranslateAnimation(0, 0,
+//                0, endY);
+//        translateAnimationY.setInterpolator(new AccelerateInterpolator());//速率
+////        translateAnimationY.setInterpolator(new LinearInterpolator());//速率
+//        translateAnimationY.setRepeatCount(0);// 动画重复执行的次数
+//        translateAnimationX.setFillAfter(true);
+//
+//        AnimationSet set = new AnimationSet(false);
+//        set.setFillAfter(false);
+//        set.addAnimation(translateAnimationY);
+//        set.addAnimation(translateAnimationX);
+//        set.setDuration(AnimationTime1);// 动画的执行时间
+//        view.startAnimation(set);
+//        // 动画监听事件
+//        set.setAnimationListener(new Animation.AnimationListener() {
+//            // 动画的开始
+//            @Override
+//            public void onAnimationStart(Animation animation) {
+//                v.setVisibility(View.VISIBLE);
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animation animation) {
+//                // TODO Auto-generated method stub
+//            }
+//
+//            // 动画的结束
+//            @Override
+//            public void onAnimationEnd(Animation animation) {
+//                v.setVisibility(View.GONE);
+////                buyNumView.setText(buyNum + "");//
+////                buyNumView.setBadgePosition(BadgeView.POSITION_TOP_RIGHT);
+////                buyNumView.show();
+//            }
+//        });
+//    }
 
 }
