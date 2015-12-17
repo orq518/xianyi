26cfcc8e7d791e683ca17b9ccca3c75c8cdf39b5
欧瑞强 <ouruiqiang@ucfgroup.com>
2015-12-17 15:33:05 +0800

    修改发现动画

--- app/src/main/java/com/xianyi/fragment/FindFragment.java
+++ app/src/main/java/com/xianyi/fragment/FindFragment.java
@@ -60,7 +60,7 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
 
     int AnimationTime = 30000;//动画时间
     int AnimationTime1 = 800;//动画时间
-    ImageView shopTop;
+    TextView shopTop;
     FrameLayout animation_rootlayout;
     int width, height;
     /**
@@ -87,9 +87,6 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (null == mRootView) {
             mRootView = getLayoutInflater(savedInstanceState).inflate(R.layout.fragment_layout_find, null);
-            TitleView mTitleView = (TitleView) mRootView.findViewById(R.id.title);
-            mTitleView.setTitle("发现");
-            mTitleView.setLeftVisiable(false);
 
         }
         WindowManager wm = (WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE);
@@ -140,7 +137,7 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
                     ImageView qiqiu = new ImageView(mContext);// buyImg是动画的图片，我的是一个小球（R.drawable.sign）
                     int index = result % 5;
                     qiqiu.setImageResource(qiqiuID[index]);// 设置buyImg的图片
-                    setAnim(qiqiu, start_location);// 开始执行动画
+                    setAnim1(qiqiu, start_location);// 开始执行动画
                     FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) qiqiu.getLayoutParams();
                     maxWidth = 2 * params.width + 30;
                     mHandler.sendEmptyMessageDelayed(1, boomTime);
@@ -190,7 +187,7 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
         animation_rootlayout.measure(0, 0);
         width = animation_rootlayout.getWidth();
         height = animation_rootlayout.getHeight();
-        shopTop = (ImageView) mRootView.findViewById(R.id.im_top);
+        shopTop = (TextView) mRootView.findViewById(R.id.title_right);
 //        im_Bottom = (TextView) mRootView.findViewById(R.id.im_Bottom);
     }
 
@@ -268,76 +265,23 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
         popupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
             @Override
             public void onDismiss() {
+                int[] start_location = new int[2];// 一个整型数组，用来存储按钮的在屏幕的X、Y坐标
+                v.getLocationInWindow(start_location);// 这是获取购买按钮的在屏幕的X、Y坐标（这也是动画开始的坐标）
+                ImageView  buyImg = new ImageView(mContext);// buyImg是动画的图片，我的是一个小球（R.drawable.sign）
+                buyImg.setImageResource(R.drawable.sign);// 设置buyImg的图片
+                setAnim(buyImg, start_location);// 开始执行动画
+
+
                 animation_rootlayout.removeView(v);
+
             }
         });
-//
     }
 
-    //    /**
-//     * @param
-//     * @return void
-//     * @throws
-//     * @Description: 创建动画层
-//     */
-//    private ViewGroup createAnimLayout() {
-//        ViewGroup rootView = (ViewGroup) mContext.getWindow().getDecorView();
-//        LinearLayout animLayout = new LinearLayout(mContext);
-//        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
-//                LinearLayout.LayoutParams.MATCH_PARENT,
-//                LinearLayout.LayoutParams.MATCH_PARENT);
-//        animLayout.setLayoutParams(lp);
-//        animLayout.setId(Integer.MAX_VALUE);
-//        animLayout.setBackgroundResource(android.R.color.transparent);
-//        rootView.addView(animLayout);
-//        return animLayout;
-//    }
-//
-    private void setAnim(final View mView, int[] start_location) {
-//        if (anim_mask_layout == null) {
-//        anim_mask_layout = null;
-//        anim_mask_layout = createAnimLayout();
-//        anim_mask_layout.addView(mView);//把动画小球添加到动画层
-
-//        }
+    private void setAnim1(final View mView, int[] start_location) {
         animation_rootlayout.addView(mView);//把动画小球添加到动画层
         View view = addViewToAnimLayout(mView,
                 start_location);
-//        int[] end_location = new int[2];// 这是用来存储动画结束位置的X、Y坐标
-//        titleView.getLocationInWindow(end_location);// shopCart是那个购物车
-//
-//        // 计算位移
-//        int endY = end_location[1] - start_location[1];// 动画位移的y坐标
-//        final TranslateAnimation translateAnimationY = new TranslateAnimation(0, 0,
-//                0, endY);
-//        translateAnimationY.setInterpolator(new LinearInterpolator());//速率匀速
-//        translateAnimationY.setRepeatCount(0);// 动画重复执行的次数
-//
-//        final AnimationSet set = new AnimationSet(false);
-//        set.setFillAfter(false);
-//        set.addAnimation(translateAnimationY);
-//        set.setDuration(AnimationTime);// 动画的执行时间
-//
-//        view.startAnimation(set);
-//
-//        // 动画监听事件
-//        set.setAnimationListener(new Animation.AnimationListener() {
-//            // 动画的开始
-//            @Override
-//            public void onAnimationStart(Animation animation) {
-//            }
-//
-//            @Override
-//            public void onAnimationRepeat(Animation animation) {
-//            }
-//
-//            // 动画的结束
-//            @Override
-//            public void onAnimationEnd(Animation animation) {
-//                animation_rootlayout.removeView(mView);
-//                LogUtil.d("动画结束:" + animation_rootlayout.getChildCount());
-//            }
-//        });
         mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -345,28 +289,6 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
                 LogUtil.d("111animation_rootlayout.getHeight():" + animation_rootlayout.getChildCount());
                 mView.setTag("1");
                 showPopUp(view);
-//                animation_rootlayout.removeView(mView);
-//                int[] start_location = new int[2];// 一个整型数组，用来存储按钮的在屏幕的X、Y坐标
-//                mView.getLocationInWindow(start_location);// 这是获取购买按钮的在屏幕的X、Y坐标（这也是动画开始的坐标）
-//                Log.d("ouou", "start_location[0]:" + start_location[0]);
-//                Log.d("ouou", "start_location[1]:" + start_location[1]);
-//                ImageView buyImg = new ImageView(mContext);// buyImg是动画的图片，我的是一个小球（R.drawable.sign）
-//                buyImg.setImageResource(R.drawable.sign);// 设置buyImg的图片
-//                setAnim_AddTop(buyImg, start_location);
-//                translateAnimationY.cancel();
-//                mView.setVisibility(View.GONE);
-
-
-//                LogUtil.d("222animation_rootlayout.getHeight():" + animation_rootlayout.getChildCount());
-//                int count = animation_rootlayout.getChildCount();
-//                for (int i = 0; i < count; i++) {
-//
-//                    View qqview = animation_rootlayout.getChildAt(i);
-//                    int[] location = new int[2];// 这是用来存储动画结束位置的X、Y坐标
-//                    qqview.getLocationInWindow(location);// shopCart是那个购物车
-//                    LogUtil.d("位置:" + location[0] + "     " + location[1]);
-//
-//                }
             }
         });
     }
@@ -386,63 +308,93 @@ public class FindFragment extends BaseFragment implements View.OnClickListener {
         view.setLayoutParams(lp);
         return view;
     }
-//
-//    private void setAnim_AddTop(final View v, int[] start_location) {
-////        if (anim_mask_layout == null) {
-//        anim_mask_layout = null;
-//        anim_mask_layout = createAnimLayout();
-//        anim_mask_layout.addView(v);//把动画小球添加到动画层
-////        }
-//        View view = addViewToAnimLayout(v,
-//                start_location);
-//        int[] end_location = new int[2];// 这是用来存储动画结束位置的X、Y坐标
-//        titleView.getLocationInWindow(end_location);// shopCart是那个购物车
-//        Log.d("ouou", "##end_location[1]:" + end_location[1]);
-//        // 计算位移
-//        int endX = start_location[0];// 动画位移的X坐标
-//        Log.d("ouou", "##endX:" + endX);
-//        int endY = end_location[1] - start_location[1];// 动画位移的y坐标
-//        TranslateAnimation translateAnimationX = new TranslateAnimation(0,
-//                endX, 0, 0);
-//        translateAnimationX.setInterpolator(new LinearInterpolator());
-//        translateAnimationX.setRepeatCount(0);// 动画重复执行的次数
-//        translateAnimationX.setFillAfter(true);
-//
-//        TranslateAnimation translateAnimationY = new TranslateAnimation(0, 0,
-//                0, endY);
-//        translateAnimationY.setInterpolator(new AccelerateInterpolator());//速率
-////        translateAnimationY.setInterpolator(new LinearInterpolator());//速率
-//        translateAnimationY.setRepeatCount(0);// 动画重复执行的次数
-//        translateAnimationX.setFillAfter(true);
-//
-//        AnimationSet set = new AnimationSet(false);
-//        set.setFillAfter(false);
-//        set.addAnimation(translateAnimationY);
-//        set.addAnimation(translateAnimationX);
-//        set.setDuration(AnimationTime1);// 动画的执行时间
-//        view.startAnimation(set);
-//        // 动画监听事件
-//        set.setAnimationListener(new Animation.AnimationListener() {
-//            // 动画的开始
-//            @Override
-//            public void onAnimationStart(Animation animation) {
-//                v.setVisibility(View.VISIBLE);
-//            }
-//
-//            @Override
-//            public void onAnimationRepeat(Animation animation) {
-//                // TODO Auto-generated method stub
-//            }
-//
-//            // 动画的结束
-//            @Override
-//            public void onAnimationEnd(Animation animation) {
-//                v.setVisibility(View.GONE);
-////                buyNumView.setText(buyNum + "");//
-////                buyNumView.setBadgePosition(BadgeView.POSITION_TOP_RIGHT);
-////                buyNumView.show();
-//            }
-//        });
-//    }
 
+
+
+
+    /**
+     * @Description: 创建动画层
+     * @param
+     * @return void
+     * @throws
+     */
+    private ViewGroup createAnimLayout() {
+        ViewGroup rootView = (ViewGroup) mContext.getWindow().getDecorView();
+        LinearLayout animLayout = new LinearLayout(mContext);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT);
+        animLayout.setLayoutParams(lp);
+        animLayout.setId(Integer.MAX_VALUE);
+        animLayout.setBackgroundResource(android.R.color.transparent);
+        rootView.addView(animLayout);
+        return animLayout;
+    }
+
+    private View addViewToAnimLayout(final ViewGroup vg, final View view,
+                                     int[] location) {
+        int x = location[0];
+        int y = location[1];
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.leftMargin = x;
+        lp.topMargin = y;
+        view.setLayoutParams(lp);
+        return view;
+    }
+
+    private void setAnim(final View v, int[] start_location) {
+        anim_mask_layout = null;
+        anim_mask_layout = createAnimLayout();
+        anim_mask_layout.addView(v);//把动画小球添加到动画层
+        final View view = addViewToAnimLayout(anim_mask_layout, v,
+                start_location);
+        int[] end_location = new int[2];// 这是用来存储动画结束位置的X、Y坐标
+        shopTop.getLocationInWindow(end_location);// shopCart是那个购物车
+
+        // 计算位移
+        int endX = end_location[0] - start_location[0] + 40;// 动画位移的X坐标
+        int endY = end_location[1] - start_location[1];// 动画位移的y坐标
+        LogUtil.d("11endX:"+endX);
+        LogUtil.d("11endY:"+endY);
+        TranslateAnimation translateAnimationX = new TranslateAnimation(0,
+                endX, 0, 0);
+        translateAnimationX.setInterpolator(new LinearInterpolator());
+        translateAnimationX.setRepeatCount(0);// 动画重复执行的次数
+        translateAnimationX.setFillAfter(true);
+
+        TranslateAnimation translateAnimationY = new TranslateAnimation(0, 0,
+                0, endY);
+        translateAnimationY.setInterpolator(new AccelerateInterpolator());
+        translateAnimationY.setRepeatCount(0);// 动画重复执行的次数
+        translateAnimationX.setFillAfter(true);
+
+        AnimationSet set = new AnimationSet(false);
+        set.setFillAfter(false);
+        set.addAnimation(translateAnimationY);
+        set.addAnimation(translateAnimationX);
+        set.setDuration(800);// 动画的执行时间
+        view.startAnimation(set);
+        // 动画监听事件
+        set.setAnimationListener(new Animation.AnimationListener() {
+            // 动画的开始
+            @Override
+            public void onAnimationStart(Animation animation) {
+                v.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+                // TODO Auto-generated method stub
+            }
+
+            // 动画的结束
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                v.setVisibility(View.GONE);
+            }
+        });
+
+    }
 }
